a8912e31c0af382654d146608a6eaf01869137cf
G.K <152147320+gklad@users.noreply.github.com>
2023-11-27 22:00:10 +1100

    Phishing Educator files uploaded :)

---
+++ application.py
@@ -0,0 +1,107 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import messagebox
+import random
+import json
+from questions import questions
+
+class AntiPhishingApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Anti-Phishing Educator")
+        ctk.set_default_color_theme("green")
+        self.geometry("350x250")
+
+        # Login screen
+        self.login_frame = ctk.CTkFrame(self)
+        self.login_frame.pack(fill='both', expand=True)
+
+        self.username_entry = ctk.CTkEntry(self.login_frame, placeholder_text="Employee ID")
+        self.username_entry.pack(pady=20)
+
+        self.password_entry = ctk.CTkEntry(self.login_frame, placeholder_text="Password", show="*")
+        self.password_entry.pack(pady=20)
+
+        self.login_button = ctk.CTkButton(self.login_frame, text="Login", command=self.on_login)
+        self.login_button.pack(pady=20)
+
+        self.creator_label_login = ctk.CTkLabel(self.login_frame, text="Created by Grigorios Kladakis")
+        self.creator_label_login.pack(side='bottom')
+
+    def on_login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if username == "13902972" and password == "test":
+            self.initialize_tool(username)
+            self.geometry("650x500")
+        else:
+            messagebox.showerror("Login Failed", "Incorrect Employee ID or password.")
+
+    def initialize_tool(self, username):
+        self.login_frame.pack_forget()
+        self.score = 0
+
+        self.label = ctk.CTkLabel(self, text="Welcome to the Anti-Phishing Educator!", font=("Helvetica", 14))
+        self.label.pack(pady=20)
+
+        self.question_label = ctk.CTkLabel(self, text="", font=("Helvetica", 12))
+        self.question_label.pack(pady=20)
+
+        self.creator_label_app = ctk.CTkLabel(self, text="Created by Grigorios Kladakis")
+        self.creator_label_app.pack(side='bottom', anchor='w')
+
+        self.options = []
+        for i in range(4):
+            btn = ctk.CTkButton(self, text="", command=lambda i=i: self.check_answer(i))
+            btn.pack(pady=10)
+            self.options.append(btn)
+
+        self.explanation_label = ctk.CTkLabel(self, text="", font=("Helvetica", 10))
+        self.explanation_label.pack(pady=20)
+
+        self.next_button = ctk.CTkButton(self, text="Next Question", command=self.load_question)
+        self.next_button.pack(pady=20)
+
+        self.load_question()
+
+    def load_question(self):
+        if questions:  # Ensure the questions list is not empty
+            self.current_question = random.choice(questions)
+            self.question_label.configure(text=self.current_question["question"])
+            for i, option in enumerate(self.current_question["options"]):
+                self.options[i].configure(text=option)
+            self.explanation_label.configure(text="")
+            self.next_button.pack_forget()
+
+    def check_answer(self, i):
+        correct = i == int(self.current_question["correct_answer"]) - 1
+        if correct:
+            self.score += 1
+            messagebox.showinfo("Correct!", "Well done. The explanation can be found below.")
+            self.explanation_label.configure(text=self.current_question["explanation"], wraplength=500)
+            self.next_button.pack(pady=20)
+
+            if self.score == 8:
+                self.complete_quiz()
+        else:
+            messagebox.showinfo("Incorrect", "Please read the question carefully and try again.")
+            self.explanation_label.configure(text="")
+            self.next_button.pack_forget()
+
+    def complete_quiz(self):
+        for widget in self.winfo_children():
+            widget.pack_forget()
+
+        self.geometry("300x150")
+
+        completion_label = ctk.CTkLabel(self, text="Thank you for completing the daily quiz!", font=("Helvetica", 14))
+        completion_label.pack(pady=20)
+
+        exit_button = ctk.CTkButton(self, text="Exit Application", command=self.quit)
+        exit_button.pack(pady=20)
+
+if __name__ == "__main__":
+    app = AntiPhishingApp()
+    app.mainloop()
---
+++ questions.py
@@ -0,0 +1,62 @@
+questions = [
+    {
+        "question": "What is the significance of a padlock icon next to a website's URL in your web browser?",
+        "options": ["It indicates that the website is the top result in a search engine", "It signifies that the website has no advertisements", "It shows that the website is using a secure, encrypted connection (HTTPS)", "It means the website is certified by a cybersecurity agency"],
+        "correct_answer": "3",
+        "explanation": "A padlock icon next to a website's URL in a web browser indicates that the site is using a secure, encrypted connection (HTTPS). This is important for protecting sensitive data, but it doesn't guarantee the site is not a phishing or malicious site."
+    },
+    {
+        "question": "What is the safest action to take when you receive an email with a hyperlink from an unknown sender?",
+        "options": ["Click on the link to verify its destination", "Forward the email to your colleagues", "Ignore the email", "Report the email to your IT security team"],
+        "correct_answer": "4",
+        "explanation": "When you receive an email with a hyperlink from an unknown sender, the safest action is to report it to your IT security team. Clicking on the link can lead to a phishing site, and forwarding the email can spread potential threats to colleagues."
+    },
+    {
+        "question": "What should you do if you receive an email that appears to be from a senior executive asking for confidential company information?",
+        "options": ["Reply with the requested information", "Verify the request through a separate communication channel", "Ignore the email", "Forward the email to other colleagues for their input"],
+        "correct_answer": "2",
+        "explanation": "Phishers may impersonate senior executives to elicit sensitive information. Always verify such email requests through a separate communication channel, like a phone call or in-person meeting, before responding."
+    },
+    {
+        "question": "What is a common sign of a phishing email?",
+        "options": ["Urgent language", "Spelling errors", "Unfamiliar sender", "All of the above"],
+        "correct_answer": "4",
+        "explanation": "Phishing emails often contain urgent language, spelling errors, and come from unfamiliar senders."
+    },
+    {
+        "question": "What should you do if you receive an unexpected email attachment from an unknown sender?",
+        "options": ["Open it immediately", "Forward it to your manager", "Delete the email", "Contact the sender for verification"],
+        "correct_answer": "3",
+        "explanation": "Unexpected email attachments, especially from unknown senders, can contain malware or links to phishing sites. It's safest to delete such emails without opening the attachment."
+    },
+    {
+        "question": "How can you identify a phishing link in an email?",
+        "options": ["By the sender's email address", "Checking for spelling errors in the URL", "Hovering over the link to preview the URL", "All of the above"],
+        "correct_answer": "4",
+        "explanation": "Phishing links can be identified by checking the sender's email address for authenticity, looking for misspellings in the URL, and hovering over the link to preview the actual URL before clicking."
+    },
+    {
+        "question": "What is a common tactic used by phishers when asking for personal information?",
+        "options": ["Requesting immediate action", "Promising a reward", "Pretending to be a government official", "All of the above"],
+        "correct_answer": "4",
+        "explanation": "Phishers often use tactics such as creating a sense of urgency, promising rewards, or impersonating authority figures to trick individuals into divulging personal information."
+    },
+    {
+        "question": "What should you be cautious of on social media to avoid phishing?",
+        "options": ["Friend requests from unknown people", "Messages asking for money", "Links to 'exclusive' offers", "All of the above"],
+        "correct_answer": "4",
+        "explanation": "On social media, be wary of friend requests from strangers, messages that ask for money or personal details, and links to exclusive offers that seem too good to be true, as these can be phishing attempts."
+    },
+    {
+        "question": "What is the best practice if you receive a suspicious call asking for personal information?",
+        "options": ["Provide the information", "Hang up immediately", "Ask for their credentials", "Call back on an official number"],
+        "correct_answer": "4",
+        "explanation": "If you receive a suspicious call, do not provide personal information. Hang up and call back using an official number from the organization's website to verify the caller's identity."
+    },
+    {
+        "question": "Why is it important to ensure a website uses HTTPS before entering sensitive information?",
+        "options": ["HTTPS sites load faster", "HTTPS indicates official content", "HTTPS encrypts data sent to the site", "All websites use HTTPS"],
+        "correct_answer": "3",
+        "explanation": "HTTPS is important because it encrypts the data sent between your browser and the website, making it more difficult for hackers to intercept sensitive information."
+    },
+]
